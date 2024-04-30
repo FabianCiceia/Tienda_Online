6f00c21b8e0e01cb59642978dc994692e079942c
FacundoMM <facundoezem@gmail.com>
2024-04-30 18:17:35 -0400

    Arreglado de input y comienzo de pagina para usuario

--- client/src/components/Navbar/Logout.jsx
+++ client/src/components/Navbar/Logout.jsx
@@ -2,23 +2,29 @@ import React, { useContext } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import UserContext from '../../context/UserContext';
+import { useNavigate } from 'react-router-dom';
+
+
 function Logout() {
+    const navigate = useNavigate()
+
+
     const baseURL = "http://localhost:8000/api/auth/logout";
     const { setUser } = useContext(UserContext);
     const logoutUser = async () => {
         try {
-            await axios.post("http://localhost:8000/api/auth/logout",
+            await axios.post(`${baseURL}`,       //Le puse el base Url
                 { withCredentials: true }
             );
             localStorage.removeItem("user");
             setUser(null)
-            navigate("/login")
+            navigate("/login") 
         } catch (err) {
             console.log("Error: ", err)
         }
     }
     const handleLogout = () => {
-        logoutUser();
+        logoutUser();                       
     };
 
     return (
--- client/src/views/PruebaComponente.jsx
+++ client/src/views/PruebaComponente.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useContext } from 'react';
+import UserContext from '../context/UserContext';
+import Axios from "../hooks/useAxios"
+import CardCart from '../components/CartCard';
+
 
 function PruebaComponente() {
+
+  const { data, isLoading, error, setData } = Axios("http://localhost:8000/api/auth/cart/list"); //Datos de el carrito... Devuelve un array donde el id es la variable producto
+
+  const ProductosCard = () => {
+    const id = products.product
+    const { dataProducto , isLoading, errorProducto, setData } = Axios(`http://localhost:8000/api/product/${id}`);
+  }
+
+
+
+
+  if (isLoading) {
+    return (
+      <div>Cargando</div>
+    )
+  }
+  if (error) {
+    return (
+      <div>Esto va tardar mas de lo pensado</div>
+    )
+  }
+  if (data) {
+    // console.log(data)
+  }
+
+  // const { data, isLoading, error, setData } = Axios(`http://localhost:8000/api/product/${id}`); Obtiene los datos de el producto con el id de arriba
+
+  const { user } = useContext(UserContext);
+  // console.log(user)
   return (
     <div>
-      
+      Estas en el componente de prueba
+      {
+        data.cart.map((data, i) => (
+          <div key={i}>
+            {/* {data.quantity} */}
+            <CardCart setProduct={setData} products={data} />
+          </div>
+        ))
+      }
     </div>
   )
 }
