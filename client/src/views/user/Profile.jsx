eb7b836c0b55cc9d35adf5c00abad8e103078282
FacundoMM <facundoezem@gmail.com>
2024-04-30 23:07:41 -0400

    Footer sin estilado y inicio de profile

--- client/src/views/user/Profile.jsx
+++ client/src/views/user/Profile.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import '../../styles/user/Profile.css'
 
-function Profile() {
-    return (
-        <div>
-            perfil usuario
-        </div>
-    )
+import React from 'react'
+import { useContext } from 'react';
+import UserContext from '../../context/UserContext';
+import { CiUser } from 'react-icons/ci';
+
+
+
+function PruebaComponente() {
+  const { user } = useContext(UserContext);
+
+  console.log(user)
+  return (
+    <div className='container userCard'>
+      <div className='userCard_head'>       
+        <CiUser className='userImage'/> 
+        <h2>{user.firstName}{user.lastName}</h2>
+      </div>
+      <div className='userCard_body'>
+        <p>Gmail: {user.email}</p>
+        <p>Phone: none</p>
+        <p>Referencias: none</p>
+      </div>
+      <div className='userCard_footer'>
+        <button type="button" className='button-primary'>Editar</button>
+      </div>
+    </div>
+  )
 }
 
-export default Profile
+export default PruebaComponente
