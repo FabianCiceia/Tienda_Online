adab65f57e7f03b8f1475e368baeab51efa76b7a
FacundoMM <facundoezem@gmail.com>
2024-04-30 20:10:44 -0400

    Nuevas pruebas para componentes

--- client/src/views/PruebaComponente.jsx
+++ client/src/views/PruebaComponente.jsx
@@ -6,48 +6,14 @@ import CardCart from '../components/CartCard';
 
 
 function PruebaComponente() {
+  // const { user } = useContext(UserContext);
 
-  const { data, isLoading, error, setData } = Axios("http://localhost:8000/api/auth/cart/list"); //Datos de el carrito... Devuelve un array donde el id es la variable producto
+return (
+  <div>
+    Estas en el componente de prueba
 
-  const ProductosCard = () => {
-    const id = products.product
-    const { dataProducto , isLoading, errorProducto, setData } = Axios(`http://localhost:8000/api/product/${id}`);
-  }
-
-
-
-
-  if (isLoading) {
-    return (
-      <div>Cargando</div>
-    )
-  }
-  if (error) {
-    return (
-      <div>Esto va tardar mas de lo pensado</div>
-    )
-  }
-  if (data) {
-    // console.log(data)
-  }
-
-  // const { data, isLoading, error, setData } = Axios(`http://localhost:8000/api/product/${id}`); Obtiene los datos de el producto con el id de arriba
-
-  const { user } = useContext(UserContext);
-  // console.log(user)
-  return (
-    <div>
-      Estas en el componente de prueba
-      {
-        data.cart.map((data, i) => (
-          <div key={i}>
-            {/* {data.quantity} */}
-            <CardCart setProduct={setData} products={data} />
-          </div>
-        ))
-      }
-    </div>
-  )
+  </div>
+)
 }
 
 export default PruebaComponente
