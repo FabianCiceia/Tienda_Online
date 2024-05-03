756afbd9540517add07998e87caa9c5cf6fd5222
FabianCiceia <ciceiafabian@gmail.com>
2024-05-03 01:49:33 -0400

    build

--- client/src/components/HomeListCategory.jsx
+++ client/src/components/HomeListCategory.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Axios from "../hooks/useAxios"
 import { Link } from 'react-router-dom';
 import { CiSignpostR1 } from "react-icons/ci";
+import { Drawer } from 'antd';
 
 function HomeListCategory() {
     const { data, isLoading, error, setData } = Axios("http://localhost:8000/api/product/list/category");
+    const [open,setOpen] = useState(false);
+    const onClose = () => {
+        setOpen(false);
+        filter();
+    };
+    const showDrawer = () => {
+        setOpen(true);
+    };
     if (isLoading){
         return(
             <div>Cargando</div>
@@ -19,16 +28,38 @@ function HomeListCategory() {
     //     console.log(data)
     // }
     return (
-        <div className='category container'>
-            <h1>Categorias</h1>
-            {
-                data.map((data, i) => (
-                    <div key={i} className='categoryLink'>
-                        <CiSignpostR1/>
-                        <Link to={`/store/category/${data}`} >{data}</Link>
-                    </div>
-                ))
-            }
+        <div>
+            <div className='category container'>
+                <h1>Categorias</h1>
+                {
+                    data.map((data, i) => (
+                        <div key={i} className='categoryLink'>
+                            <CiSignpostR1/>
+                            <Link to={`/store/category/${data}`} >{data}</Link>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className='categoryMovile'>
+                <button className='button-primary' onClick={showDrawer}>Categorias</button>
+                <Drawer
+                    title="Categorias"
+                    placement={'top'}
+                    closable={false}
+                    onClose={onClose}
+                    visible={open}
+                    key={'left'}
+                >
+                    {
+                        data.map((data, i) => (
+                            <div key={i} className='categoryLink'>
+                                <CiSignpostR1/>
+                                <Link to={`/store/category/${data}`} >{data}</Link>
+                            </div>
+                        ))
+                    }
+                </Drawer>
+            </div>
         </div>
     )
 }
